edbe381473cc1d436e7cd7f70aedcf0ec11ab269
Boran Kuşcu <boran7200@gmail.com>
2023-12-21 11:19:30 +0300

    data layer init

---
+++ eduflow/src/main/java/com/final_project/datalayer/faculty.java
@@ -0,0 +1,36 @@
+package com.final_project.datalayer;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+
+@Entity
+public class Faculty {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long facultyId;
+
+    @Column(nullable = false)
+    private String name;
+
+    // getters and setters
+
+    public Long getFacultyId() {
+        return facultyId;
+    }
+
+    public void setFacultyId(Long facultyId) {
+        this.facultyId = facultyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
