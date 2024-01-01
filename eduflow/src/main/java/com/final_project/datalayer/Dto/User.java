4d7a0e0a502ebe4439ab498b9e16c23f39c878e5
BBoranN <92054300+BBoranN@users.noreply.github.com>
2024-01-01 19:16:23 +0300

    dto

---
+++ eduflow/src/main/java/com/final_project/datalayer/Dto/User.java
@@ -0,0 +1,18 @@
+package com.final_project.datalayer.Dto;
+
+public class User {
+    
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String role;
+
+    private int studentId;
+
+    private String phone;
+
+    private String token;
+}
