164a8ed236115bf2ebf98150b57d37b4d2693726
BBoranN <92054300+BBoranN@users.noreply.github.com>
2024-01-05 20:08:11 +0300

    fix

--- eduflow/src/main/java/com/final_project/jwt/SecurityConfig.java
+++ eduflow/src/main/java/com/final_project/jwt/SecurityConfig.java
@@ -1,5 +1,12 @@
 package com.final_project.jwt;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     
+
 }
