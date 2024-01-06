f8b8363211789a572043a9267f756f368cdd4378
BBoranN <92054300+BBoranN@users.noreply.github.com>
2024-01-07 00:28:14 +0300

    controller base

---
+++ eduflow/src/main/java/com/final_project/eduflow/RequestController.java
@@ -0,0 +1,42 @@
+package com.final_project.eduflow;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+
+@RestController
+public class RequestController {
+    
+    @GetMapping("/RequestParams")
+    public void getRequestParams(){
+
+    }
+
+    @GetMapping("/AdviserRequest")
+    public void getAdviserRequest(){
+
+    }
+
+    @GetMapping("/StudentRequest")
+    public void getStudentRequest(){
+
+    }
+
+    @PostMapping("/makeRequest")
+    public void makeRequest(){
+
+    }
+    
+    @PutMapping("path/{id}")
+    public void updateRequest(@PathVariable String id){
+
+    }
+
+}
