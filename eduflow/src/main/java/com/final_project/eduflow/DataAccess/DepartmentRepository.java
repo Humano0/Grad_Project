e13309e6a5952bffffb6bb296708d950063fc1d0
magurow <ozandede01@gmail.com>
2024-01-08 21:28:55 +0300

    half of entity repos

---
+++ eduflow/src/main/java/com/final_project/eduflow/DataAccess/DepartmentRepository.java
@@ -0,0 +1,12 @@
+package com.final_project.eduflow.DataAccess;
+
+import com.final_project.eduflow.Data.Entities.Course;
+import com.final_project.eduflow.Data.Entities.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long > {
+    Department findByDepartmentId(Long departmentId);
+    Department findByFacultyId(Integer facultyId);
+    Department findByName(String name);
+
+}
--- eduflow/src/main/java/com/final_project/eduflow/DataAccess/SectionRecordsRepository.java
+++ eduflow/src/main/java/com/final_project/eduflow/DataAccess/SectionRecordsRepository.java
@@ -1,6 +1,6 @@
 package com.final_project.eduflow.DataAccess;
 
-import com.final_project.eduflow.Data.Entities.Section;
+
 import com.final_project.eduflow.Data.Entities.SectionRecords;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +8,5 @@ public interface SectionRecordsRepository extends JpaRepository<SectionRecords, 
     SectionRecords findBySectionNumber(Integer sectionNumber);
     SectionRecords findByCourseCode(String CourseCode);
     SectionRecords findByTerm(String term);
-    
+
 }
