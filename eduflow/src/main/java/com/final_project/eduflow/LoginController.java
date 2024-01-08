ffd992aaef0f765cf15a3f2bb48a2ffb18dc2bd1
BBoranN <92054300+BBoranN@users.noreply.github.com>
2024-01-08 18:45:35 +0300

    fuck

--- eduflow/src/main/java/com/final_project/eduflow/LoginController.java
+++ eduflow/src/main/java/com/final_project/eduflow/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.final_project.DataAccess.Repositories.StaffRepository;
 import com.final_project.datalayer.Dto.LoginUser;
 import com.final_project.datalayer.Dto.User;
 import com.final_project.services.Interfaces.IUserService;
@@ -18,22 +19,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 public class LoginController {
     
-    private final IUserService userService;
+    private final StaffRepository staffRepository;
 
     @Autowired
-    public LoginController(IUserService userService) {
-        this.userService = userService;
+    public LoginController(StaffRepository staffRepository) {
+        this.staffRepository = staffRepository;
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> Login(@RequestBody LoginUser entity) {
-        try {
-            User user = userService.findUser(entity); // Assuming userService has a login method
-            return ResponseEntity.ok(user); // Return the User object with a 200 OK status
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body( e.getMessage()); // Return the error message with a 400 Bad Request status
-        }
-       
+        return ResponseEntity.ok(staffRepository.findByEmail(entity.getEmail()));
     }
     @GetMapping("/debug")
     public String getMethodName() {
