7bb1b69421c62b021651c25f5e0c0eb7d2df3540
Eyüp Can Yönter <oniraca.om00@gmail.com>
2024-01-09 14:07:05 +0300

    changed deprecated method

--- eduflow/src/main/java/com/final_project/eduflow/Config/SecurityConfig.java
+++ eduflow/src/main/java/com/final_project/eduflow/Config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -17,7 +18,7 @@ import java.util.Arrays;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
-        http.csrf(c -> c.disable())
+        http.csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/login").permitAll()
