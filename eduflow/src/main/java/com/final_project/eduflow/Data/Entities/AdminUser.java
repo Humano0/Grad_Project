a849f4b008c47fddf573164708f78fafcc8aaa77
BBoranN <92054300+BBoranN@users.noreply.github.com>
2024-02-03 15:39:41 +0300

    admin users and whole users view

---
+++ eduflow/src/main/java/com/final_project/eduflow/Data/Entities/AdminUser.java
@@ -0,0 +1,46 @@
+package com.final_project.eduflow.Data.Entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "admin_user")
+public class AdminUser {
+    
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    //getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+}
---
+++ eduflow/src/main/java/com/final_project/eduflow/Data/View/UsersView.java
@@ -0,0 +1,64 @@
+package com.final_project.eduflow.Data.View;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users_view")
+public class UsersView {
+    
+    
+    @Column(name = "id")
+    private Long id;
+
+    @Id
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+
+    //getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+    
+}
