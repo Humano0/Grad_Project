17adf0e05d0b2741a6707831781ef78be282d1f8
BBoranN <92054300+BBoranN@users.noreply.github.com>
2024-02-10 21:07:48 +0300

    fix

--- eduflow/src/main/java/com/final_project/eduflow/DataAccess/StudentRequestRepository.java
+++ eduflow/src/main/java/com/final_project/eduflow/DataAccess/StudentRequestRepository.java
@@ -6,8 +6,9 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.final_project.eduflow.Data.Entities.StudentRequests;
+import com.final_project.eduflow.Data.Entities.IdClasses.StudentRequestsId;
 
-public interface StudentRequestRepository  extends JpaRepository<StudentRequests, Long>{
+public interface StudentRequestRepository  extends JpaRepository<StudentRequests, StudentRequestsId>{
     List<StudentRequests> findByStudentId(long studentId);
     Optional<StudentRequests> findByStudentIdAndRequestTypeIdAndWhen(long studentId, long requestTypeId, OffsetDateTime when);
 }
--- eduflow/src/main/java/com/final_project/eduflow/DataAccess/StudentRequestsListingViewRepository.java
+++ eduflow/src/main/java/com/final_project/eduflow/DataAccess/StudentRequestsListingViewRepository.java
@@ -1,11 +1,13 @@
 package com.final_project.eduflow.DataAccess;
 
 import com.final_project.eduflow.Data.View.StudentRequestsListingView;
+import com.final_project.eduflow.Data.View.IdClasses.StudentRequestsListingViewId;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface StudentRequestsListingViewRepository extends JpaRepository<StudentRequestsListingView, Long> {
+public interface StudentRequestsListingViewRepository extends JpaRepository<StudentRequestsListingView, StudentRequestsListingViewId> {
     List<StudentRequestsListingView> findStudentRequestsListingViewByStudentId(Long studentId);
 
 }
