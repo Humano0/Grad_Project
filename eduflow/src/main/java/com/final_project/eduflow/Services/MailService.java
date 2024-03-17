0ae62073eddd48ac0908f50e4fed2b1f419b60b2
Eyüp Can Yönter <oniraca.om00@gmail.com>
2024-03-17 08:29:28 +0300

    some changes

--- eduflow/src/main/java/com/final_project/eduflow/Services/MailService.java
+++ eduflow/src/main/java/com/final_project/eduflow/Services/MailService.java
@@ -1,35 +1,43 @@
-//package com.final_project.eduflow.Services;
-//
-//
-//
-//
-//import com.final_project.eduflow.Data.DTO.MailEntity;
-//import com.final_project.eduflow.Services.Interfaces.IMailService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class MailService implements IMailService {
-//
-//    @Autowired
-//    private JavaMailSender mailSender;
-//
-//    @Value("$(spring.mail.username)")
-//    private String fromMail;
-//
-//    public void sendMail(String mail, MailEntity mailEntity)
-//    {
-//        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-//        simpleMailMessage.setFrom("fromMail");
-//        simpleMailMessage.setSubject(mailEntity.getSubject());
-//        simpleMailMessage.setText(mailEntity.getMessage());
-//        simpleMailMessage.setTo(mail);
-//
-//        mailSender.send(simpleMailMessage);
-//    }
-//
-//}
-//
+package com.final_project.eduflow.Services;
+
+
+
+
+import com.final_project.eduflow.Data.DTO.MailEntity;
+import com.final_project.eduflow.Services.Interfaces.IMailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService implements IMailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("$(spring.mail.username)")
+    private String fromMail;
+
+    @Override
+    public void sendMail(String mail, MailEntity mailEntity) {
+
+    }
+
+    @Override
+    public void sendRequestAcceptedMailtoStudent(String studentMail) {
+
+    }
+
+    @Override
+    public void sendRequestRejectedMailtoStudent(String studentMail) {
+
+    }
+
+    @Override
+    public void sendRequestWaitingMailtoStaff(String staffMail) {
+
+    }
+}
+
